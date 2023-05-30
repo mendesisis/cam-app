3c7584d80c59c0aba309b796809e612af1c08145
mendesisis <vantaeisis@gmail.com>
2023-05-30 20:58:50 -0300

    salvandoo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { Camera, CameraType } from 'expo-camera';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { Image } from 'expo-image';
 
 export default function App() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
@@ -12,14 +13,17 @@ useEffect(() =>{
   (async () => {
     const cameraStatus = await Camera.requestCameraPermissionsAsync();
     setHasCameraPermission(cameraStatus.status === 'granted');
+    await MediaLibrary.requestPermissionsAsync();
   })();
 }, []);
 
 async function takePicture (){
   if (camera) {
 
-  const photo = await camera.takePictureAsync();
-  console.log(photo.uri);
+  const {uri} = await camera.takePictureAsync();
+  console.log(uri);
+  setImage(uri);
+  await MediaLibrary.saveToLibraryAsync(uri);
  }
 }
   
@@ -33,10 +37,23 @@ async function takePicture (){
       ratio={'1:1'}
     
     />
-     <Button  
-     title="" 
-     onPress={() => { takePicture()}} />
+    <Image 
+    style={styles.container}
+    source={image}
+    contentFit='cover'
+    transition={1000}
 
+    />
+     <TouchableHighlight 
+       style={styles.button}
+       onPress={() => { takePicture()}}
+       >
+        <Text
+        style={{color:'#fff', fontSize:25}}>
+          Tirar Foto
+         
+        </Text>
+     </TouchableHighlight>
   </View>
   );
 }
@@ -48,9 +65,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image :{
+    flex: 1,
+    width: '100%',
+    height:'100%'
+
+  },
   fixedRatio: {
     aspectRatio: 1,
     flex: 1
+
+  },
+  button: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    elevation: 15,
+    backgroundColor: 'black',
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    position:'absolute',
+    bottom: 50
 
   }
 });
