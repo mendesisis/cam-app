8e0a445f9a6495839bdfeb4fc17c599990417c40
mendesisis <vantaeisis@gmail.com>
2023-05-29 21:39:43 -0300

    salvar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
+import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const[camera, setCamera] = useState<Camera>(null);
+  const[image, setImage] = useState (null);
+  
+useEffect(() =>{
+  (async () => {
+    const cameraStatus = await Camera.requestCameraPermissionsAsync();
+    setHasCameraPermission(cameraStatus.status === 'granted');
+  })();
+}, []);
+
+async function takePicture (){
+  if (camera) {
+
+  const photo = await camera.takePictureAsync();
+  console.log(photo.uri);
+ }
+}
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera
+      ref={(ref) =>setCamera(ref)}
+      style={styles.fixedRatio}
+      type={CameraType.back}
+      ratio={'1:1'}
+    
+    />
+     <Button  
+     title="" 
+     onPress={() => { takePicture()}} />
+
+  </View>
   );
 }
 
@@ -17,4 +48,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fixedRatio: {
+    aspectRatio: 1,
+    flex: 1
+
+  }
 });
